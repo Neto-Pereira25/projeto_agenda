10abb11e5922aad6589e4e071105db886bd19d37
Jose Neto <devneto203@gmail.com>
2024-06-07 09:51:14 -0300

    created the login/create account page

--- routes.js
+++ routes.js
@@ -1,28 +1,13 @@
 const express = require('express');
 const route = express.Router();
+
 const homeController = require('./src/controllers/homeController');
-const contatoController = require('./src/controllers/contatoController');
-
-function meuMiddleware(req, res, next) {
-    req.session = { nome: 'Neto', sobrenome: 'Pereira' };
-    console.log();
-    console.log('Passei no seu middleware.');
-    console.log();
-    next();
-}
-
-// a rota vai decidir qual controller vai contralar uma determinada
-// rota.
-
-// o trabalho do controller é decidir qual o model e qual o view vai ser usado
-
+const loginController = require('./src/controllers/loginController');
 
 // Rotas da home 
-route.get('/', homeController.paginaInicial);
-route.post('/', homeController.trataPost);
+route.get('/', homeController.index);
 
-// Rotas de contato
-route.get('/contato', contatoController.paginaInicial);
-
+// Rotas de login
+route.get('/login/index', loginController.index);
 
 module.exports = route;
