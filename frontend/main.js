f33f39df182421251cdfa71f6fd68401a28dde6f
Jose Neto <devneto203@gmail.com>
2024-08-18 03:54:44 -0300

    preparação para o deploy

--- frontend/main.js
+++ frontend/main.js
@@ -1,5 +1,13 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-import './assets/css/style.css';
+import Login from './modules/Login'
+
+const login = new Login('.from-login');
+const cadastro = new Login('.from-cadastro');
+
+login.init();
+cadastro.init();
+
+// import './assets/css/style.css';
 
